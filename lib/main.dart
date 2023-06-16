c287442f3a52f2447e036c0a351484f6f60a5a34
Adrian Edwards <adrian@adriancedwards.com>
2023-06-16 16:36:51 -0700

    remove constant for a now stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const ErgPageView(),
+      home: ErgPageView(),
     );
   }
 }
