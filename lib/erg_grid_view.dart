8a8a98c627d6ce80367004f9e22e218d47790b4a
Adrian Edwards <adrian@adriancedwards.com>
2023-06-15 22:24:43 -0700

    make  the grid view stateless

--- lib/erg_grid_view.dart
+++ lib/erg_grid_view.dart
@@ -1,37 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:openergview/data_tile.dart';
 
-class ErgGridView extends StatefulWidget {
-  const ErgGridView({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<ErgGridView> createState() => _ErgGridViewState();
-}
-
-class _ErgGridViewState extends State<ErgGridView> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ErgGridView extends StatelessWidget {
+  const ErgGridView({super.key});
 
   @override
   Widget build(BuildContext context) {
