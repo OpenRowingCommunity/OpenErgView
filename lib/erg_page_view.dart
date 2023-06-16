c2d52a2ec6764dfbf4ec79d3a98cff921e4f73f4
Adrian Edwards <adrian@adriancedwards.com>
2023-06-16 16:36:23 -0700

    set up theming of bottom navigation bar

--- lib/erg_page_view.dart
+++ lib/erg_page_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:openergview/constants.dart';
 import 'package:openergview/erg_grid_view.dart';
 
 class ErgPageView extends StatefulWidget {
@@ -33,18 +34,28 @@ class _ErgPageViewState extends State<ErgPageView> {
           ErgGridView()
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.star), label: "1"),
-          BottomNavigationBarItem(icon: Icon(Icons.star), label: "2"),
-          BottomNavigationBarItem(icon: Icon(Icons.star), label: "3")
-        ],
-        type: BottomNavigationBarType.fixed,
-        onTap: (newIndex) {
-          controller.animateToPage(newIndex,
-              duration: Duration(milliseconds: 500), curve: Curves.ease);
-        },
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          gradient: getDarkGradient(context),
+          color: Colors.indigo,
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.white,
+          selectedIconTheme: IconThemeData(color: Colors.white),
+          currentIndex: _currentIndex,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: "1"),
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: "2"),
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: "3")
+          ],
+          type: BottomNavigationBarType.fixed,
+          onTap: (newIndex) {
+            controller.animateToPage(newIndex,
+                duration: Duration(milliseconds: 500), curve: Curves.ease);
+          },
+        ),
       ),
     );
   }
