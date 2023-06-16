0aea92d78d3c1e5a5381f81589877c80a4700ae8
Adrian Edwards <adrian@adriancedwards.com>
2023-06-15 22:24:53 -0700

    remoev app bar of grid view

--- lib/erg_grid_view.dart
+++ lib/erg_grid_view.dart
@@ -13,15 +13,6 @@ class ErgGridView extends StatelessWidget {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
         body: GridView.count(
             crossAxisCount: 3,
             padding: const EdgeInsets.all(4),
