9cef64c96ad2b8c4ace5ef8a6a8eed4dc34d6f4d
Adrian Edwards <adrian@adriancedwards.com>
2023-08-18 21:49:13 -0400

    add SafeArea to erg page view

--- lib/erg_page_view.dart
+++ lib/erg_page_view.dart
@@ -60,163 +60,170 @@ class _ErgPageViewState extends State<ErgPageView>
   Widget build(BuildContext context) {
     final PageController pageController = PageController(initialPage: 0);
 
-    return Scaffold(
-        body: PageView(
-          /// [PageView.scrollDirection] defaults to [Axis.horizontal].
-          /// Use [Axis.vertical] to scroll vertically.
-          controller: pageController,
-          onPageChanged: (newIndex) {
-            setState(() {
-              _currentIndex = newIndex;
-              tabController.animateTo(newIndex);
-            });
-          },
-          children: <Widget>[
-            ErgGridView(
-              children: [
-                DataTile(
-                    title: "distance",
-                    defaultValue: 1,
-                    stream: widget.erg
-                        .monitorForData({"general.distance"}).map((event) {
-                      var data = event["general.distance"] as double;
-                      print(data);
-                      return data;
-                    })),
-                DataTile(title: "test", defaultValue: 2),
-                DataTile(title: "test", defaultValue: 3),
-                DataTile(title: "test", defaultValue: 14),
-                DataTile(title: "test", defaultValue: 15),
-                DataTile(title: "test", defaultValue: 16)
+    return SafeArea(
+        child: Scaffold(
+            body: PageView(
+              /// [PageView.scrollDirection] defaults to [Axis.horizontal].
+              /// Use [Axis.vertical] to scroll vertically.
+              controller: pageController,
+              onPageChanged: (newIndex) {
+                setState(() {
+                  _currentIndex = newIndex;
+                  tabController.animateTo(newIndex);
+                });
+              },
+              children: <Widget>[
+                ErgGridView(
+                  children: [
+                    DataTile(
+                        title: "distance",
+                        defaultValue: 1,
+                        stream: widget.erg
+                            .monitorForData({"general.distance"}).map((event) {
+                          var data = event["general.distance"] as double;
+                          print(data);
+                          return data;
+                        })),
+                    DataTile(title: "test", defaultValue: 2),
+                    DataTile(title: "test", defaultValue: 3),
+                    DataTile(title: "test", defaultValue: 14),
+                    DataTile(title: "test", defaultValue: 15),
+                    DataTile(title: "test", defaultValue: 16)
+                  ],
+                ),
+                Center(
+                  child: Text('First Page'),
+                ),
+                ErgGridView(
+                  children: [
+                    DataTile(title: "test", defaultValue: 1),
+                    DataTile(title: "test", defaultValue: 2),
+                    DataTile(title: "test", defaultValue: 3),
+                    DataTile(title: "test", defaultValue: 4),
+                    DataTile(title: "test", defaultValue: 5),
+                    DataTile(title: "test", defaultValue: 6)
+                  ],
+                )
               ],
             ),
-            Center(
-              child: Text('First Page'),
-            ),
-            ErgGridView(
-              children: [
-                DataTile(title: "test", defaultValue: 1),
-                DataTile(title: "test", defaultValue: 2),
-                DataTile(title: "test", defaultValue: 3),
-                DataTile(title: "test", defaultValue: 4),
-                DataTile(title: "test", defaultValue: 5),
-                DataTile(title: "test", defaultValue: 6)
-              ],
-            )
-          ],
-        ),
-        bottomNavigationBar: Container(
-          height: 58,
-          decoration: BoxDecoration(
-            gradient: getDarkGradient(context),
-            color: Colors.indigo,
-          ),
-          child: BottomAppBar(
-            color: Colors.transparent,
-            child: IconTheme(
-                data: IconThemeData(
-                    color: Theme.of(context).colorScheme.onPrimary),
-                child: Row(
-                  children: <Widget>[
-                    StreamBuilder<ErgometerConnectionState>(
-                        stream: _ergConnectionStatusStream,
-                        initialData: lastConnectionState,
-                        builder: (BuildContext context,
-                            AsyncSnapshot<ErgometerConnectionState> snapshot) {
-                          if (snapshot.hasError) {
-                            return const Text(
-                              "Error",
-                              style:
-                                  TextStyle(fontSize: 16.0, color: Colors.red),
-                            );
-                          } else {
-                            switch (snapshot.connectionState) {
-                              case ConnectionState.none:
+            bottomNavigationBar: Container(
+              height: 58,
+              decoration: BoxDecoration(
+                gradient: getDarkGradient(context),
+                color: Colors.indigo,
+              ),
+              child: BottomAppBar(
+                color: Colors.transparent,
+                child: IconTheme(
+                    data: IconThemeData(
+                        color: Theme.of(context).colorScheme.onPrimary),
+                    child: Row(
+                      children: <Widget>[
+                        StreamBuilder<ErgometerConnectionState>(
+                            stream: _ergConnectionStatusStream,
+                            initialData: lastConnectionState,
+                            builder: (BuildContext context,
+                                AsyncSnapshot<ErgometerConnectionState>
+                                    snapshot) {
+                              if (snapshot.hasError) {
                                 return const Text(
-                                  "none",
+                                  "Error",
                                   style: TextStyle(
-                                      fontSize: 16.0, color: Colors.white),
+                                      fontSize: 16.0, color: Colors.red),
                                 );
-                              case ConnectionState.waiting:
-                                return const CircularProgressIndicator();
-                              case ConnectionState.active:
-                              case ConnectionState.done:
-                                switch (snapshot.data) {
-                                  case ErgometerConnectionState.connecting:
+                              } else {
+                                switch (snapshot.connectionState) {
+                                  case ConnectionState.none:
                                     return const Text(
-                                      "Connecting...",
-                                      style: TextStyle(
-                                          fontSize: 16.0, color: Colors.yellow),
-                                    );
-                                  case ErgometerConnectionState.connected:
-                                    return const Text(
-                                      "Connected to erg",
-                                      style: TextStyle(
-                                          fontSize: 16.0, color: Colors.green),
-                                    );
-                                  case ErgometerConnectionState.disconnected:
-                                    return const Text(
-                                      "Disconnected",
-                                      style: TextStyle(
-                                          fontSize: 16.0, color: Colors.red),
-                                    );
-                                  default:
-                                    return const Text(
-                                      "Unknown",
+                                      "none",
                                       style: TextStyle(
                                           fontSize: 16.0, color: Colors.white),
                                     );
+                                  case ConnectionState.waiting:
+                                    return const CircularProgressIndicator();
+                                  case ConnectionState.active:
+                                  case ConnectionState.done:
+                                    switch (snapshot.data) {
+                                      case ErgometerConnectionState.connecting:
+                                        return const Text(
+                                          "Connecting...",
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              color: Colors.yellow),
+                                        );
+                                      case ErgometerConnectionState.connected:
+                                        return const Text(
+                                          "Connected to erg",
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              color: Colors.green),
+                                        );
+                                      case ErgometerConnectionState
+                                            .disconnected:
+                                        return const Text(
+                                          "Disconnected",
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              color: Colors.red),
+                                        );
+                                      default:
+                                        return const Text(
+                                          "Unknown",
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              color: Colors.white),
+                                        );
+                                    }
                                 }
-                            }
-                          }
-                        }),
-                    if (isPointerDevice(context))
-                      IconButton(
-                        tooltip: 'Previous',
-                        icon: const Icon(Icons.arrow_back),
-                        disabledColor: Colors.grey,
-                        onPressed: _currentIndex != 0
-                            ? () => setState(() {
-                                  pageController.animateToPage(
-                                      _currentIndex - 1,
-                                      duration: Duration(milliseconds: 500),
-                                      curve: Curves.ease);
-                                })
-                            : null,
-                      ),
-                    TabPageSelector(
-                      color: Colors.grey,
-                      borderStyle: BorderStyle.none,
-                      selectedColor: Colors.white,
-                      controller: tabController,
-                    ),
-                    if (isPointerDevice(context))
-                      IconButton(
-                        tooltip: 'Next',
-                        icon: const Icon(Icons.arrow_forward),
-                        disabledColor: Colors.grey,
-                        onPressed: _currentIndex != _pageCount - 1
-                            ? () => setState(() {
-                                  pageController.animateToPage(
-                                      _currentIndex + 1,
-                                      duration: Duration(milliseconds: 500),
-                                      curve: Curves.ease);
-                                })
-                            : null,
-                      ),
-                    const Spacer(),
-                    IconButton(
-                      tooltip: 'Settings',
-                      icon: const Icon(Icons.settings),
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => SettingsPageView()));
-                      },
-                    ),
-                  ],
-                )),
-          ),
-        ));
+                              }
+                            }),
+                        if (isPointerDevice(context))
+                          IconButton(
+                            tooltip: 'Previous',
+                            icon: const Icon(Icons.arrow_back),
+                            disabledColor: Colors.grey,
+                            onPressed: _currentIndex != 0
+                                ? () => setState(() {
+                                      pageController.animateToPage(
+                                          _currentIndex - 1,
+                                          duration: Duration(milliseconds: 500),
+                                          curve: Curves.ease);
+                                    })
+                                : null,
+                          ),
+                        TabPageSelector(
+                          color: Colors.grey,
+                          borderStyle: BorderStyle.none,
+                          selectedColor: Colors.white,
+                          controller: tabController,
+                        ),
+                        if (isPointerDevice(context))
+                          IconButton(
+                            tooltip: 'Next',
+                            icon: const Icon(Icons.arrow_forward),
+                            disabledColor: Colors.grey,
+                            onPressed: _currentIndex != _pageCount - 1
+                                ? () => setState(() {
+                                      pageController.animateToPage(
+                                          _currentIndex + 1,
+                                          duration: Duration(milliseconds: 500),
+                                          curve: Curves.ease);
+                                    })
+                                : null,
+                          ),
+                        const Spacer(),
+                        IconButton(
+                          tooltip: 'Settings',
+                          icon: const Icon(Icons.settings),
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => SettingsPageView()));
+                          },
+                        ),
+                      ],
+                    )),
+              ),
+            )));
   }
 
   @override
