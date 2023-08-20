1da5a72bd93d1aff16e0bb50a7a0a9b247b4c1c2
Adrian Edwards <adrian@adriancedwards.com>
2023-08-20 12:02:24 -0400

    wrap the whole app in the ErgometerStore provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:openergview/src/ergometerstore.dart';
+import 'package:provider/provider.dart';
 
 import 'devices_list/devices_bloc_provider.dart';
 import 'devices_list/devices_list_view.dart';
@@ -7,7 +9,7 @@ import 'erg_grid_view.dart';
 import 'erg_page_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Provider(create: (_) => ErgometerStore(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
