c3ac718fb4f5dab7950d0f9248e662bc560b96ce
Adrian Edwards <adrian@adriancedwards.com>
2023-08-19 21:02:31 -0400

    more alignment and fit adjustments to the staggered view

--- lib/erg_staggered_view.dart
+++ lib/erg_staggered_view.dart
@@ -11,35 +11,45 @@ class ErgStaggeredView extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     // account for bottom bar height and padding
-    final double itemHeight = (size.height - 80 - 4) / 2;
-    final double barHeight = (size.height - 80 - 4) / 4;
+    double spaceHeight = size.height - kBottomNavigationBarHeight;
+    final double itemHeight = (spaceHeight) / 2 - 12;
 
     final double itemWidth = size.width / 3;
     return Scaffold(
         body: Row(
       children: <Widget>[
         Container(
-            width: size.width * 2 / 3,
+            width: itemWidth * 2,
+            height: spaceHeight,
             padding: const EdgeInsets.fromLTRB(2, 2, 0, 2),
-            child: Column(children: <Widget>[
-              Row(children: <Widget>[
-                Expanded(
-                    flex: 2, child: DataBar(defaultValue: 2000, unit: "m")),
-                Expanded(flex: 1, child: DataBar(defaultValue: 32, unit: "s/m"))
-              ]),
-              Row(children: <Widget>[
-                Expanded(child: DataBar(defaultValue: 2000, unit: "/500m"))
-              ]),
-              Row(children: <Widget>[
-                Expanded(flex: 2, child: DataBar(defaultValue: 2000)),
-                Expanded(
-                    flex: 1,
-                    child: DataBar(icon: Icons.favorite, defaultValue: 32))
-              ]),
-              Row(children: <Widget>[
-                Expanded(child: DataBar(defaultValue: 2000, unit: "ave/500"))
-              ]),
-            ])),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(children: <Widget>[
+                    Expanded(
+                        flex: 2, child: DataBar(defaultValue: 2000, unit: "m")),
+                    Expanded(
+                        flex: 1, child: DataBar(defaultValue: 32, unit: "s/m"))
+                  ]),
+                  Row(children: <Widget>[
+                    Expanded(
+                        child: DataBar(
+                      defaultValue: 2000,
+                      unit: "/500m",
+                      fontSize: 64,
+                    ))
+                  ]),
+                  Row(children: <Widget>[
+                    Expanded(flex: 2, child: DataBar(defaultValue: 2000)),
+                    Expanded(
+                        flex: 1,
+                        child: DataBar(icon: Icons.favorite, defaultValue: 32))
+                  ]),
+                  Row(children: <Widget>[
+                    Expanded(
+                        child: DataBar(defaultValue: 2000, unit: "ave/500"))
+                  ]),
+                ])),
         SizedBox(
           width: itemWidth,
           child: GridView.count(
